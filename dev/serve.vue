c50cdcd2737e1f95aebd8866dbfef92a83784677
Alan Morel <alan@alanmorel.com>
2020-09-27 20:49:36 -0400

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <vida-sample />
+    </div>
+</template>
+
+<script>
+    import Vue from "vue";
+    import { VidaSample } from "@/entry";
+
+    export default Vue.extend({
+        name: "ServeDev",
+        components: {
+            VidaSample
+        }
+    });
+</script>
+
---
+++ src/components.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VidaSample } from "@/components/vida-sample.vue";
