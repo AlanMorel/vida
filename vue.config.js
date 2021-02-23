6247620638a242c97ec937e7d5341c01dbe07c07
Alan Morel <alan@alanmorel.com>
2021-02-23 17:05:52 -0500

    Misc file clean ups

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,6 @@
-const path = require("path");
-
 module.exports = {
     productionSourceMap: false,
-    css: { 
+    css: {
         extract: false
-    },
-    chainWebpack: config => {
-        config.resolve.alias.set("@", path.resolve(__dirname, "src"));
     }
 };
