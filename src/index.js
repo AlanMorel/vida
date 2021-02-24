a09e3698dc841ae4a9ebd4502fa2a2f6d6d7e5e1
Alan Morel <alan@alanmorel.com>
2021-02-24 17:11:04 -0500

    Export validation list

--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ export { default as SpinnerIcon } from "@/components/icons/SpinnerIcon";
 export { default as FormInput } from "@/components/utility/FormInput";
 export { default as ProgressButton } from "@/components/utility/ProgressButton";
 export { default as StrengthIndicator } from "@/components/utility/StrengthIndicator";
+export { default as ValidationList } from "@/components/utility/ValidationList";
 export { default as Vida } from "@/components/Vida";
