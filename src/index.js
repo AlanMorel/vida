a9681e0613e43c7da1ccc711e784e900fa052212
Alan Morel <alan@alanmorel.com>
2021-02-23 22:29:04 -0500

    Export FormInput

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+export { default as EyeClosedIcon } from "@/components/icons/EyeClosedIcon";
+export { default as EyeOpenedIcon } from "@/components/icons/EyeOpenedIcon";
 export { default as Icon } from "@/components/icons/Icon";
 export { default as SpinnerIcon } from "@/components/icons/SpinnerIcon";
-export { default as EyeOpenedIcon } from "@/components/icons/EyeOpenedIcon";
-export { default as EyeClosedIcon } from "@/components/icons/EyeClosedIcon";
-
+export { default as FormInput } from "@/components/utility/FormInput";
